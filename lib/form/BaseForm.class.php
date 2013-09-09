167972ec42a7e026a87025b85b845b26dc0449a9
PatBriPerso <patbriperso@hotmail.com>
2013-09-09 23:30:12 +0200

    Generate project and app
    Commands:
    php lib/vendor/symfony/data/bin/symfony generate:project rrd-monitor
    "PatBriPerso"
    php symfony plugin:publish-assets
    php symfony generate:app frontend

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    rrd-monitor
+ * @subpackage form
+ * @author     PatBriPerso 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
